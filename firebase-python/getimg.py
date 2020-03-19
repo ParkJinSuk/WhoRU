8373b9998ff8480afb3e136b065a9d2a67367d9e
HeeJung Hong <48550633+HongHeeJung@users.noreply.github.com>
2020-03-19 22:11:01 +0900

    customized face recognition

--- firebase-python/getimg.py
+++ firebase-python/getimg.py
@@ -16,22 +16,21 @@ sr_buck = 'whoru-ed991.appspot.com'
 cred = credentials.Certificate("./whoru-ed991-firebase-adminsdk-o6sq7-2aa9f68fea.json")
 db_app = firebase_admin.initialize_app(cred, {'databaseURL': db_url})
 sr_app = firebase_admin.initialize_app(cred, {'storageBucket': sr_buck,}, name='storage')
-# ref = db.reference('/whoru-ed991')
 ref = db.reference()
 
 
 def find_user():
-    usr = ref.child('10주 1300/FLAG').get()
-    print(usr)
-    if usr == 1:
-        print(usr)
+    rd = ref.child('10주 1300/Request Detection/').get()
+    if rd == 1:
+        username = ref.child('10주 1300/username/').get()
+        print(username)
 
 
-find_user()
 bucket = storage.bucket(app=sr_app)
 blob = bucket.blob("WhoRU_target/jacob.jpg")
 img_url = blob.generate_signed_url(datetime.timedelta(seconds=300), method='GET')
 # print(img_url)
-
-urllib.request.urlretrieve(img_url, './image/1.jpg')
+find_user()
+urllib.request.urlretrieve(img_url, './knn_examples/train/{}.jpg'.format(username))
 print("save")
+
