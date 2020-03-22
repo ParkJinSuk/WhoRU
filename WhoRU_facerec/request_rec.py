8578d3ef7319f2bff37e4f1b52456f63e9215414
HeeJung Hong <48550633+HongHeeJung@users.noreply.github.com>
2020-03-22 14:48:41 +0900

    Update request_rec.py

--- WhoRU_facerec/request_rec.py
+++ WhoRU_facerec/request_rec.py
@@ -3,7 +3,6 @@
 2020.03.21
 버튼이 눌리면 firebase database에 있는 'Request' 값을 1로 바꾸고,
 firebase database의 Request 값이 1인 차량 번호를 불러온다.
-(미완)
 '''
 
 import firebase_admin
@@ -25,15 +24,9 @@ ref = db.reference()
 while True:
     input_state = GPIO.input(11)
     if input_state is False:
-        ref.child("06수 8850").update({'Request': '1'})
+        ref.child("carlist/06수 8850").update({'Request': '1'})
         GPIO.output(12, GPIO.HIGH)
     else:
         GPIO.output(12, GPIO.LOW)
-
-    query = ref.order_by_child('Request').equal_to('1')
-    snapshot = query.get()
-    for key in snapshot:
-        print(key)
-    car_number = ref.child("Request").get()
 
     time.sleep(0.5)
